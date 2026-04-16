2b5d90e3b348c57e33440b6a76afa4031675701a
hystericallygreat-ops <hysterically.great@gmail.com>
2026-04-16 16:38:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(page_title="Dashboard SaaS Ventes", layout="wide")
+st.set_page_config(page_title="Dashboard Ventes", layout="wide")
 
 # ---------------- STYLE ----------------
 st.markdown("""
